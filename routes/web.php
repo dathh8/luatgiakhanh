3c21214bf8231b99ac156747e429044b5e060522
khanhly2702 <hoanghuudat2309@gmail.com>
2022-11-06 11:44:19 +0700

    add admin-module

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage');
 });
-Route::get('admin', function () {
-    return view('admin');
-});
+// Route::get('admin', function () {
+//     return view('admin');
+// });
